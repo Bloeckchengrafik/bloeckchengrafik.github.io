2c2ea02727f33f734035a343c8075d1856ebe72b
Christian Bergschneider <magiclake@gmx.de>
2024-04-25 12:16:08 +0000

    feat: main design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://bloeckchengrafik.de',
+  integrations: [mdx(), sitemap(), tailwind(), svelte()]
 });
---
+++ src/components/EduFlow.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import {writable} from 'svelte/store';
+    import {SvelteFlow, Background, type Node as XYNode, type Edge, Position} from '@xyflow/svelte';
+
+    import '@xyflow/svelte/dist/style.css';
+
+    const nodes = writable<XYNode[]>([
+        {
+            id: '1',
+            position: {x: 100, y: 140},
+            data: {
+                label: 'Goethe-Gymnasium Bensheim'
+            },
+            selected: true,
+            sourcePosition: Position.Right,
+            targetPosition: Position.Left
+        },
+        {
+            id: '2',
+            position: {x: 300, y: 150},
+            data: {
+                label: 'h_da Darmstadt'
+            },
+            sourcePosition: Position.Right,
+            targetPosition: Position.Left
+        },
+        {
+            id: '3',
+            position: {x: 500, y: 150},
+            data: {label: 'Not unlocked yet...'},
+            sourcePosition: Position.Right,
+            targetPosition: Position.Left,
+            class: "opacity-50"
+        }
+    ]);
+    const edges = writable<Edge[]>([
+            {
+                id: '1-2',
+                source: '1',
+                target: '2',
+                animated: true
+            },
+            {
+                id: '2-3',
+                source: '2',
+                target: '3',
+            }
+        ]
+    );
+</script>
+
+<main>
+    <SvelteFlow {nodes} {edges} zoomOnScroll={false} autoPanOnConnect={false} autoPanOnNodeDrag={false}
+                nodesConnectable={false} nodeOrigin={[0.5, 0.5]}>
+        <Background bgColor="#f8fbf8"/>
+        <!--        <Controls />-->
+    </SvelteFlow>
+</main>
+
+<style>
+    main {
+        height: 300px;
+        width: 100%;
+    }
+</style>
